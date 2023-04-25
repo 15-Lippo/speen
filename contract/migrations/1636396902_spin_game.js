325872e0d04d424a1541c6781b1a5f6efcbb5489
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2023-04-25 16:47:24 +0200

    Update 1636396902_spin_game.js

--- contract/migrations/1636396902_spin_game.js
+++ contract/migrations/1636396902_spin_game.js
@@ -4,6 +4,6 @@ module.exports = function (_deployer, network, accounts) {
   console.log("accounts", accounts);
   const owner = accounts[0];
   const numberConbfirmationRequired = 2;
-  const usdtAddr = "0x3b00ef435fa4fcff5c209a37d1f3dcff37c705ad";
+  const usdtAddr = "0xD0355200111C2B21AAbC1a31552eCCDc5d4E905d";
   _deployer.deploy(spinProccessor, usdtAddr);
 };
